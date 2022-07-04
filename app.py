d404eda84212f5309df37ba9b42c74ff26d4e902
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2022-07-04 10:10:33 -0700

    adjust openai input query and response
    - allow arbitrary queries and extend length of openai result in response

--- app.py
+++ app.py
@@ -10,26 +10,30 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        query = request.form["query"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
+            # prompt=generate_prompt(animal),
+            prompt=query,
             temperature=0.6,
+            n=1,
+            max_tokens=1000,
         )
+        print(response.choices)
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+# def generate_prompt(animal):
+#     return """Suggest three names for an animal that is a superhero.
+#
+# Animal: Cat
+# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+# Animal: Dog
+# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+# Animal: {}
+# Names:""".format(
+#         animal.capitalize()
+#     )
