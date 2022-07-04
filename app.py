36a91c35326fcb2679589aaec8c046a1f483d31e
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2022-07-04 10:15:21 -0700

    get rid of commented out code

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def index():
         query = request.form["query"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            # prompt=generate_prompt(animal),
             prompt=query,
             temperature=0.6,
             n=1,
@@ -24,16 +23,3 @@ def index():
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
-
-
-# def generate_prompt(animal):
-#     return """Suggest three names for an animal that is a superhero.
-#
-# Animal: Cat
-# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-# Animal: Dog
-# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-# Animal: {}
-# Names:""".format(
-#         animal.capitalize()
-#     )
